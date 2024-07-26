79fe557e43c3bedce6807e34cdb5c9d354cfbde4
MatLeg <mat.legierskii@gmail.com>
2024-07-26 17:31:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/retrokeyboard/Config.kt
@@ -0,0 +1,66 @@
+package com.example.retrokeyboard
+
+object Config {
+
+    val COLUMNS_NUMBER = 3
+
+    val KEYS = mapOf(
+        1 to listOf('x'),
+        2 to listOf('a','b','c'),
+        3 to listOf('d','e','f'),
+        4 to listOf('g','h','i'),
+        5 to listOf('j','k','l'),
+        6 to listOf('m','n','o'),
+        7 to listOf('p','q','r','s'),
+        8 to listOf('t','u','v'),
+        9 to listOf('w','x','y','z'),
+        11 to listOf('*'),
+        12 to listOf(','),
+        13 to listOf('#')
+    )
+
+}
+
+
+class RetroKeyboard() {
+    private val row1 = listOf(
+        Key(symbol = '1', chars = listOf('ć')),
+        Key(symbol = '2', chars = listOf('a','b','c')),
+        Key(symbol = '3', chars = listOf('d','e','f')),
+    )
+    private val row2 = listOf(
+        Key(symbol = '4', chars = listOf('g','h','i')),
+        Key(symbol = '5', chars = listOf('j','k','l')),
+        Key(symbol = '6', chars = listOf('m','n','o')),
+    )
+    private val row3 = listOf(
+        Key(symbol = '7', chars = listOf('p','q','r','s')),
+        Key(symbol = '8', chars = listOf('t','u','v')),
+        Key(symbol = '9', chars = listOf('w','x','y','z')),
+    )
+    private val row4 = listOf(
+        Key(symbol = ' ', chars = listOf('*')),
+        Key(symbol = '0', chars = listOf(',')),
+        Key(symbol = ' ', chars = listOf('#')),
+    )
+
+    fun getChars(): Map<Int, List<Key>> {
+        return mapOf(1 to row1, 2 to row2, 3 to row3, 4 to row4)
+    }
+
+    fun getNextChar(key: Key, char: Char?): Char {
+        val chars = key.chars
+        return if (char == null) chars.first()
+        else {
+            val index = chars.indexOf(char) + 1
+            val nextIndex = index.coerceIn(0, chars.size)
+            chars.getOrNull(nextIndex) ?: chars.first()
+        }
+    }
+}
+
+
+data class Key(
+    val symbol: Char,
+    val chars: List<Char>
+)
---
+++ app/src/main/java/com/example/retrokeyboard/Keyboard.kt
@@ -0,0 +1,125 @@
+package com.example.retrokeyboard
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+
+@Composable
+fun CustomKeyboard(modifier: Modifier = Modifier) {
+    val keybaord = RetroKeyboard()
+
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp.dp
+    val keyWidthDp = screenWidthDp / 3
+
+    val coroutineScope = rememberCoroutineScope()
+    
+    var text by remember { mutableStateOf("Hello-") }
+    var selectedChar: Char? by remember { mutableStateOf(null) }
+    var typingChar: Char? by remember { mutableStateOf(null) }
+
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Column {
+            Spacer(modifier = Modifier.size(100.dp))
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Label") }
+            )
+            ///////////////
+            Column(Modifier.fillMaxWidth()) {
+                keybaord.getChars().forEach { (_, key) ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Green)
+                            .padding(12.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        key.forEach {
+                            Key(
+                                modifier = Modifier.width(keyWidthDp),
+                                key = it,
+                                selectedChar = selectedChar
+                            ) {
+                                selectedChar = keybaord.getNextChar(it, selectedChar)
+
+
+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+@Composable
+@Preview
+fun Key(
+    modifier: Modifier = Modifier,
+    key: Key = Key('2', listOf('a','b','c')),
+    selectedChar: Char? = 'c',
+    onClick: () -> Unit = {},
+) {
+    Button(modifier = modifier, onClick = { onClick() }) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = key.symbol.toString())
+            Chars(chars = key.chars, selectedChar = selectedChar)
+        }
+    }
+}
+
+@Composable
+fun Chars(modifier: Modifier = Modifier, chars: List<Char>, selectedChar: Char?) {
+    if (selectedChar !in chars) Text(text = chars.joinToString())
+    else {
+        val selectedCharIndex = chars.indexOf(selectedChar)
+        val beforeSelected = chars.subList(0, selectedCharIndex)
+        val afterSelected = chars.subList(selectedCharIndex+1, chars.size)
+        Row {
+            Text(text = beforeSelected.joinToString(""))
+            Text(
+                text = selectedChar.toString(),
+                color = Color.Blue
+            )
+            Text(text = afterSelected.joinToString())
+        }
+    }
+}
