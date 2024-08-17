e3eecbd0e5fddd63a77204ac2337409778876dd3
MatLeg <mat.legierskii@gmail.com>
2024-08-17 20:26:15 +0200

    display toast when copied text to clipboard

--- app/src/main/java/com/example/retrokeyboard/extensions/Context.kt
+++ app/src/main/java/com/example/retrokeyboard/extensions/Context.kt
@@ -3,10 +3,13 @@ package com.example.retrokeyboard.extensions
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
+import com.example.retrokeyboard.R
 
 fun Context.copyTextToClipboard(text: String) {
     val clipboard = getSystemService(this, ClipboardManager::class.java) as ClipboardManager
     val clip = ClipData.newPlainText("RetroKeyboardApp", text)
     clipboard.setPrimaryClip(clip)
+    Toast.makeText(this, getString(R.string.copied_to_clipboard), Toast.LENGTH_SHORT).show()
 }
